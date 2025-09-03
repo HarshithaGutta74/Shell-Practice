bfc3e098a0ede56743b6064603036b5432b23283
Gutta Harshitha <guttaharshitha74@gmail.com>
2025-09-03 11:31:22 +0530

    learning variables

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Trump
+PERSON2=Musk
+
+echo "$Person1: Hey $Person2, How are you?"
+echo "$Person2:: Hello Trum, I am good. How are you doing?"
+echo "$Person1: I am good too. What's up?"
+echo "$Person2:: Nothing, Just going to mars, will you come."
+echo "$Person1: Sorry, I will come once you come back"
