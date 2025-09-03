f80e9f724ec43292285f8c7dea46c82136c2cbda
Gutta Harshitha <guttaharshitha74@gmail.com>
2025-09-03 11:50:47 +0530

    learning variables

---
+++ 04-arguments.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hey $PERSON2, How are you?"
+echo "$PERSON2: Hello $PERSON1 I am good. How are you doing?"
+echo "$PERSON1: I am good too. What's up?"
+echo "$PERSON2: Nothing, Just going to mars, will you come."
+echo "$PERSON1: Sorry, I will come once you come back"
