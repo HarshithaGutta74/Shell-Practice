b9a31313d0c6911fe4060a48bd43756992f167ee
Gutta Harshitha <guttaharshitha74@gmail.com>
2025-09-03 11:23:45 +0530

    Conversation b/w Trump and musk

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+echo "Trump: Hey Musk, How are you?"
+echo "Musk:: Hello Trum, I am good. How are you doing?"
+echo "Trump: I am good too. What's up?"
+echo "Musk:: Nothing, Just going to mars, will you come."
+echo "Trump: Sorry, I will come once you come back"
